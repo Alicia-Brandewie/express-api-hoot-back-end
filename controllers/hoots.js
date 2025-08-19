25ceab8ebc944301308d766747aba8eada9a01d6
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-08-19 12:18:58 -0500

    working on show route -- 401 error

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -3,16 +3,36 @@ const verifyToken = require("../middleware/verify-token.js");
 const Hoot = require("../models/hoot.js");
 const router = express.Router();
 
-
+// POST - /hoots
 router.post("/", verifyToken, async (req, res) => {
-  try {
-    req.body.author = req.user._id;
-    const hoot = await Hoot.create(req.body);
-    hoot._doc.author = req.user;
-    res.status(201).json(oot);
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
+    try {
+        req.body.author = req.user._id;
+        const hoot = await Hoot.create(req.body);
+        hoot._doc.author = req.user;
+        res.status(201).json(oot);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// GET - index /hoots
+router.get("/", verifyToken, async (req, res) => {
+    try {
+        const hoots = await Hoot.find({}).populate("author").sort({ createdAt: "desc" });
+        res.status(200).json(hoots);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// GET - show /:hootId
+router.get("/:hootId", verifyToken, async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId).populate("author");
+        res.status(200).json(hoot);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
 });
 
 module.exports = router;
